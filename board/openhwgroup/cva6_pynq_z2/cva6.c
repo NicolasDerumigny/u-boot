f1b971bff80c80540ec87b45a1cc2dc82264673f
Nicolas Derumigny <nderumigny@gmail.com>
2025-06-17 20:11:15 +0200

    feat(pynq_z2): use board dtb

--- board/openhwgroup/cva6_pynq_z2/cva6.c
+++ board/openhwgroup/cva6_pynq_z2/cva6.c
@@ -1,9 +1,10 @@
 #include <init.h>
 
-int board_init(void) {
-  return 0;
-}
+int board_init(void) { return 0; }
 
-int ft_board_setup(void *fdt, struct bd_info *bd) {
-  return 0;
+int ft_board_setup(void *fdt, struct bd_info *bd) { return 0; }
+
+void *board_fdt_blob_setup(int *err) {
+  *err = 0;
+  return ((void *)0x82200000);
 }
